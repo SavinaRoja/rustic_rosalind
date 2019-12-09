08d6e6e21e90b13156fcfc3540973f4a1518bdb2
Paul Barton <pablo.barton@gmail.com>
2019-12-08 23:19:05 -0500

    adding fib solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod dna;
+pub mod fib;
 mod revc;
 mod rna;
 
 pub use crate::dna::dna;
+pub use crate::fib::fib;
 pub use crate::revc::revc;
 pub use crate::rna::rna;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
 use rosalind::{dna, rna, revc};
+use rosalind::fib;
 
 #[macro_use]
 extern crate clap;
 use clap::App;
-
 
 
 fn main() {
@@ -19,6 +19,10 @@ fn main() {
             let input_file = matches.subcommand_matches("dna").unwrap().value_of("input").unwrap();
             let contents = fs::read_to_string(input_file).unwrap();
             println!("{}", dna(&contents));
+        },
+        Some("fib") => {
+            let input_file = matches.subcommand_matches("fib").unwrap().value_of("input").unwrap();
+            println!("{}", fib(fib::file_parse(input_file)));
         },
         Some("rna") => {
             let input_file = matches.subcommand_matches("rna").unwrap().value_of("input").unwrap();
